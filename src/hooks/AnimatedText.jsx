cb0a3c939f11727831f8cac8e48771c57ca87eb3
Tien2003deptrai <duongvantiendtu@gmail.com>
2025-08-28 20:15:02 +0700

    Fix: layout margin text

--- src/hooks/AnimatedText.jsx
+++ src/hooks/AnimatedText.jsx
@@ -4,14 +4,14 @@ const AnimatedText = ({ text }) => {
   const letters = Array.from(text);
 
   return (
-    <div className="flex space-x-1">
+    <div className="flex flex-wrap gap-x-1 sm:gap-x-1.5 md:gap-x-2 gap-y-1 leading-none">
       {letters.map((char, index) => (
         <motion.span
           key={index}
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: index * 0.05 }}
-          className="text-emerald-400 text-5xl font-extrabold"
+          className="text-emerald-400 font-extrabold text-3xl xs:text-4xl sm:text-5xl md:text-5xl"
         >
           {char}
         </motion.span>
